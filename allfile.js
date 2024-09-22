5ccfb647577af914babe9f6fb2cb8ea20c9605d4
Ariful islam <afkgamingno1@gmail.com>
2024-09-22 15:40:51 +0600

    add all pages files

---
+++ allfile.js
@@ -0,0 +1,28 @@
+function showgallary() {
+    const showmanubg = document.getElementById('gallary_show')
+    showmanubg.style.display = 'block';
+}
+function hidegallary() {
+    const showmanubg = document.getElementById('gallary_show')
+    showmanubg.style.display = 'none';
+}
+
+// mobile manu funtion 
+function manu_show() {
+    const manubgshow = document.getElementById('manu_bg')
+    manubgshow.style.display = 'block';
+}
+function manu_hide() {
+    const manubghide = document.getElementById('manu_bg')
+    manubghide.style.display = 'none';
+}
+  
+//   // mobile manu funtion 
+//   function manu_show() {
+//     const manubgshow = document.getElementById('manu_bg')
+//     manubgshow.style.display = 'block';
+// }
+// function manu_hide() {
+//     const manubghide = document.getElementById('manu_bg')
+//     manubghide.style.display = 'none';
+// }
